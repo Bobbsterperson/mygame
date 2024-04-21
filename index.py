5fa649c8e01b1224d3505dee36d16e2a009038a1
bugston <bugstonn@gmail.com>
2024-04-21 15:09:23 +0300

    sd

--- index.py
+++ index.py
@@ -6,6 +6,9 @@ import os
 from fastapi.staticfiles import StaticFiles
 
 
+
+
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/images", StaticFiles(directory="images"), name="images")
@@ -14,16 +17,11 @@ image_filenames = os.listdir(IMAGES_FOLDER)
 initial_image_filename = "img1.jpg"
 current_image_index = 0
 
-
 @app.get("/")
 async def button(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "button": ""})
 
-@app.post("/process_left")
-async def process_left():
-    # Add your logic here to handle "left" button action
-    print("Left button clicked!")
-    return JSONResponse(content={"message": "Left button clicked successfully"})
+
 
 @app.post("/process_walk")
 async def process_walk():
@@ -34,6 +32,31 @@ async def process_walk():
     current_image_filename = image_filenames[current_image_index]
     current_image_index = (current_image_index + 1) % len(image_filenames)
     return JSONResponse(content={"initial_image_filename": initial_image_filename, "current_image_filename": current_image_filename})
+
+
+
+@app.post("/process_left")
+async def process_left():
+    global IMAGES_FOLDER, IMAGES_FOLDER2
+
+    # Toggle between images and images2 folders
+    if IMAGES_FOLDER == "images":
+
+        IMAGES_FOLDER = "images2"
+
+        app.mount("/images", StaticFiles(directory=IMAGES_FOLDER), name="images")
+        app.mount("/images2", StaticFiles(directory="images2"), name="images2")
+    else:
+        IMAGES_FOLDER = "images"
+        app.mount("/images", StaticFiles(directory="images"), name="images")
+        app.mount("/images2", StaticFiles(directory=IMAGES_FOLDER), name="images2")
+    print("Left button clicked!")
+    return JSONResponse(content={"message": "Left button clicked successfully"})
+
+
+
+
+
 
 @app.post("/process_right")
 async def process_punch():
